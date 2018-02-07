99f40f993d1c95ebea67bb4788e108283ef1d3d3
Michael-Plachter <michael.plachter@wework.com>
2018-02-07 14:49:39 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,10 @@
-puts "Hello World!"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]  
+  puts "#{board[0]}  |  #{board[1]} | #{board[2]}  "
+  puts "------------"
+  puts "#{board[3]}  |  #{board[4]} |#{board[5]}   "
+  puts "------------"   
+  puts "#{board[6]}  |  #{board[7]} | #{board[8]}  "
+end
